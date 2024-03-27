a676b4fc02ef2db943c246fb60728b8f993ed0e1
dyddks <dyddks0115@naver.com>
2024-03-27 10:26:57 +0900

    PostRequest 예제

---
+++ _03_PostRequest/src/main/java/post/login/LoginServlet.java
@@ -0,0 +1,35 @@
+package post.login;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		/* 브라우저가 보내온 데이터가 한글인 경우
+		 * 값을 꺼내기 전에 문자셋 설정을 해야한다.
+		 * get요청은 tomcat설정파일에
+		 * post요청은 이곳에서 처리한다.
+		 * */
+		req.setCharacterEncoding("UTF-8");
+		String id = req.getParameter("id");
+		String pass = req.getParameter("password");
+		
+		System.out.println("LoginServlet::doPodt => " + id + " : " + pass);
+		
+		res.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = res.getWriter();
+		
+		out.println("<html><body>");
+		out.println("<h1>로그린 결과</h1>");
+		out.println(id+"님을 환영합니다.");
+		out.println("</body><html>");
+	}
+}

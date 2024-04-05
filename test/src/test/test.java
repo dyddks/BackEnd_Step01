a171ac38b5d9e052869f46853985da3b395e7038
dyddks <dyddks0115@naver.com>
2024-04-05 11:56:13 +0900

    project_list jsp 구현

---
+++ test/src/test/test.java
@@ -0,0 +1,26 @@
+package test;
+
+import java.util.Arrays;
+
+public class test {
+	public static void main(String[] args) {
+		
+	}
+}
+
+class Solution {
+    public int[] solution(int N, int[] stages) {
+        int[] answer = {};
+        
+        return answer;
+    }
+    
+    public double fail(int[] stages){
+        Arrays.sort(stages);
+        
+        for(int stage : stages) {
+        	
+        }
+        return 1.2;
+    }
+}
